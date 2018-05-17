5f46ddb6b1397a76d7a68a9cb44d9b4046be1d5a
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-17 17:32:03 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,7 +12,7 @@ class Artist
 
  
   def initialize
-    @@artists << self
+    self.class.all << self
     @songs = []
   end
 
@@ -29,9 +29,4 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
- # def to_param
-   # name.downcase.gsub(' ', '-')
- # end
-
 end
